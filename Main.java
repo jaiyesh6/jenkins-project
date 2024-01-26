de4d94480faabf005ac1e013ce2e4493e73ecbda
jaiyesh6 <146883111+jaiyesh6@users.noreply.github.com>
2024-01-26 21:08:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi, How are you ?");
+        System.out.println("This si the first java program");
+        System.out.println(new Date());
+    }
+}
