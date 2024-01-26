60916e5df52cec908248f9d4682f9471db736787
jaiyesh6 <146883111+jaiyesh6@users.noreply.github.com>
2024-01-26 21:18:05 +0000

    2nd commit

--- Main.java
+++ Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("This si the first java program");
         System.out.println(new Date());
         System.out.println("Hello")
+        System.out.println("Now go for 2nd commit");
     }
 }
